86e06870b63ed520d9611a30c9ad9ce504da3c3d
Nika Yakubova <lilit666an@gmail.com>
2021-09-23 00:18:53 +0300

    Add formatting for number count

--- src/Components/InformationBlock/InformationBlock.tsx
+++ src/Components/InformationBlock/InformationBlock.tsx
@@ -1,25 +1,30 @@
 import React from 'react'
 import { Chip, Icon, Typography } from '@material-ui/core';
 import { useStyles } from './styles';
+import { formatNumberByStringThousand, getFormattedStringOfTitle } from '../../utils/formatting';
 interface IInformationBlockProps {
   icon: React.ReactElement,
-  title: string,
-  count?: number | string,
+  title: string | number,
+  count?: number,
   className?: string
   percent?: number
 };
 
 const InformationBlock = ({icon, count, title, percent, className = ''}: IInformationBlockProps) => {
-  const classes = useStyles()
+  const classes = useStyles();
 
-  if(percent === 0) return null
+  if(percent === 0) return null;
 
   return (
     <div className={`${classes.information} ${className} flex-center`} >
       <Icon fontSize='inherit' className={classes.icon} >{icon}</Icon>
-      <Typography variant='inherit' noWrap component='p'>{title}</Typography>
-      { count !== undefined && <Chip className={classes.chip} label={count} size='small' /> }
-      {percent !== undefined && <Typography variant='inherit' className={classes.percent} component='p' >{`${percent}%`}</Typography>  }
+      <Typography variant='inherit' noWrap component='p'>{ getFormattedStringOfTitle(title) }</Typography>
+      { count !== undefined 
+        && <Chip className={classes.chip} label={formatNumberByStringThousand(count)} size='small' /> 
+      }
+      { percent !== undefined 
+        && <Typography variant='inherit' className={classes.percent} component='p' >{`${percent}%`}</Typography>  
+      }
     </div>
   );
 };
--- src/Components/SearchCard/SearchCard.tsx
+++ src/Components/SearchCard/SearchCard.tsx
@@ -5,25 +5,27 @@ import { InformationBlock } from '../InformationBlock';
 import CircleIcon from '@mui/icons-material/Circle';
 import StarBorderIcon from '@mui/icons-material/StarBorder';
 import { useStyles } from './styles';
+import { IRepositoryPreview } from '../../Interfaces/repository';
+import { formatNumberByStringThousand } from '../../utils/formatting';
 
-const SearchCard = () => {
+const SearchCard = (props: IRepositoryPreview) => {
   const classes = useStyles();
-  
+
   return (
     <CardContent>
-    <CardTitle to='/repositories/1'>chvin/react-tetris</CardTitle>
-    <Typography className={classes.descripition}  color="textPrimary">Use React, Redux, Immutable to code Tetris. 🎮</Typography>
+    <CardTitle to={`/repositories/${props.id}`}>{props.nameWithOwner}</CardTitle>
+    <Typography className={classes.descripition}  color="textPrimary">{props.description}</Typography>
 
     <div className='flex-center' >
       <InformationBlock
         className={classes.information}
         icon={<StarBorderIcon  fontSize='inherit'/>}
-        title='1.1k'
+        title={formatNumberByStringThousand(props.stargazers?.totalCount)}
      />
      <InformationBlock
         className={classes.information}
-        icon={<CircleIcon fontSize='inherit'/>}
-        title='JavaScript'
+        icon={<CircleIcon fontSize='inherit' htmlColor={props.primaryLanguage.color} />}
+        title={props.primaryLanguage.name}
      />
     </div>
     </CardContent>

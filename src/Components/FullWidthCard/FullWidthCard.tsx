2fe720d3324bda13daa59d61bdca75fc5f857eee
Nika Yakubova <lilit666an@gmail.com>
2021-09-22 17:19:48 +0300

    Add checked loading and errors

--- src/Components/FullWidthCard/FullWidthCard.tsx
+++ src/Components/FullWidthCard/FullWidthCard.tsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import { Card } from '@material-ui/core';
 import { useStyles } from './styles';
-
+import { ApolloError } from '@apollo/client';
+import FullWidthCardSkeleton from './FullWidthCardSkeleton';
+import { ErrorMessage } from '../ErrorMessage';
 interface IFullWidthCardProps {
+  error?: ApolloError | undefined
+  loading?: boolean
   children: React.ReactNode
 }
 
-const FullWidthCard = ({children}: IFullWidthCardProps) => {
+const FullWidthCard = ({loading, error, children}: IFullWidthCardProps) => {
   const classes = useStyles()
+
+  if(loading) return <FullWidthCardSkeleton/>
+  if(error) return <ErrorMessage error={error.message} />
 
   return (
     <Card  variant='outlined' className={classes.card} >
       {children}
     </Card>
   )
-}
+};
 
 export default FullWidthCard;
